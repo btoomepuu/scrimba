6130adf9c45535e592a84213ef1e0f7ccd370239
Brennon Toomepuu <btoomepuu@gmail.com>
2023-05-09 11:45:59 -0400

    complete bbq-splash page

---
+++ first-react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>info1</li>
+        <li>info2</li>
+        <li>info3</li>
+        <li>info4</li>
+        <li>info5</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
